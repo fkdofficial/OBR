265ddd87a71bc2f0406d2be46952f03da030ae88
Febin <febdan@gmail.com>
2021-10-08 18:48:04 +0530

    Merge remote-tracking branch 'hub/master'

--- src/main/java/com/sayone/obr/controller/UserController.java
+++ src/main/java/com/sayone/obr/controller/UserController.java
@@ -1,11 +1,10 @@
 package com.sayone.obr.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
     //@PostMapping
-    //public UserController
+    public void userController() {}
 }
