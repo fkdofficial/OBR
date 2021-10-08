d861b89e68c95d779196ce522ed4c051e3d9b407
Febin Koshy Daniel <90131485+fkdofficial@users.noreply.github.com>
2021-10-08 19:50:37 +0530

    Update UserController.java

--- src/main/java/com/sayone/obr/controller/UserController.java
+++ src/main/java/com/sayone/obr/controller/UserController.java
@@ -1,10 +1,47 @@
 package com.sayone.obr.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.sayone.obr.model.request.UserDetailsRequestModel;
+import com.sayone.obr.model.response.UserRest;
+import com.sayone.obr.service.UserService;
+import com.sayone.obr.shared.dto.UserDto;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("users")
 public class UserController {
 
-    //@PostMapping
-    public void userController() {}
+    @Autowired
+    UserService userService;
+
+    @GetMapping
+    public String getUser() {
+        return "get user was called";
+    }
+
+    @PostMapping
+    public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {
+
+        UserRest returnValue = new UserRest();
+
+        UserDto userDto = new UserDto();
+        BeanUtils.copyProperties(userDetails, userDto);
+
+        UserDto createdUser = userService.createUser(userDto);
+        BeanUtils.copyProperties(createdUser, returnValue);
+
+        return returnValue;
+    }
+
+    @PutMapping
+    public String updateUser() {
+        return "update user was called";
+    }
+
+    @DeleteMapping
+    public String deleteUser() {
+        return "delete user was called";
+    }
+
 }
---
+++ src/main/java/com/sayone/obr/shared/dto/UserDto.java
@@ -0,0 +1,64 @@
+package com.sayone.obr.shared.dto;
+
+import java.io.Serializable;
+
+public class UserDto implements Serializable {
+
+
+    private static final long serialVersionUID = 5733808691880507629L;
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private int phoneNumber;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
+
