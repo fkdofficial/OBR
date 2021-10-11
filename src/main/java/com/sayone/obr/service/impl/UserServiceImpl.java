dea123d399924a861aee7cdf0319a92dd1bba32f
Febin <febdan@gmail.com>
2021-10-11 16:58:06 +0530

    Update UserController.java

---
+++ src/main/java/com/sayone/obr/service/impl/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.sayone.obr.service.impl;
+
+import com.sayone.obr.service.UserService;
+import com.sayone.obr.shared.dto.UserDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public UserDto createUser(UserDto user) {
+        return null;
+    }
+}
